846ca42adecea7073aece99b839f4a09821f7945
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2022-08-25 09:06:08 +0530

    Create count Digits.cpp

---
+++ STEP1/Know Basic Maths/count Digits.cpp
@@ -0,0 +1,17 @@
+// Problem Statement: Given an integer N , write program to count number of digits in N.
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int count_digits( int n )
+{
+  int digits = floor(log10(n) + 1);
+  return digits;
+}
+
+int main()
+{
+   int n = 12345;
+   cout<< "Number of digits in "<<n<<" is "<<count_digits(n);
+   return 0;
+}
